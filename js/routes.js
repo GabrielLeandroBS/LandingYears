ebd30fe9db42392947a8b20e265d5924cab1f970
Gabriel Leandro <48261292+GabrielLeandroBS@users.noreply.github.com>
2020-11-18 00:25:37 -0300

    Update new page

--- js/routes.js
+++ js/routes.js
@@ -1,37 +1,50 @@
-var app = angular.module("initApp", ["ngRoute"]);
+var application = angular.module("initApp", ["ngRoute"])
 
-app.config([
+application.config([
   "$routeProvider",
-  function ($routeProvider) {
-    
-    $routeProvider.when("/", {
-        title: "Index - Aulas Angular",
+   function($routeProvider){
+     $routeProvider.when("/",{
+        title: "Template 2021 - 1",
         templateUrl: "pages/main.htm",
-        controller : "newyear"
-    });
+        controller: "newyear"
+     });
 
-    $routeProvider.when("/sobre", {
-      title: "Sobre - Aulas de Angular",
-      templateUrl: "pages/sobre.htm",
-    });
+     $routeProvider.when("/year1",{
+       title: "Template 2021 - 2",
+       templateUrl: "pages/year1.htm",
+       css: "year1.css"
+     })
 
-    $routeProvider.when("/contato", {
-        title: "Contato - Aulas de Angular",
-        templateUrl: "pages/contato.htm",
-    });
-  },
-]);
+     $routeProvider.when("/contato",{
+       title: "Temaplate 2021 - 3",
+       templateUrl: "pages/contato.htm"
+     })
+   }
+])
 
-app.controller("newyear", function ($scope) {
-  $scope.newyear = "HAPPY NEW YEAR";
-});
-app.run([
+application.controller("newyear",function ($scope) {
+  $scope.newyear = "HAPPY NEW YEAR"
+})
+
+// No AngularJS o $rootScope é um objeto que permite que nós possamos armazenar todo o escopo de nossa aplicação, e em cada aplicação nós só podemos usar um e somente um $rootScope.
+// O serviço $ location analisa a URL na barra de endereço do navegador (com base em window.location ) e disponibiliza a URL para seu aplicativo
+/*
+O $broadcast chama o evento de todo mundo que está "abaixo" dele, seriam os filhos mesmo. 
+Já o $emit chama todos os eventos com aquele nome. Ele é um cara que dispara globalmente.
+O $on é o listener, então ele será ativado quando for chamado por um $emit ou $broadcast
+
+angularEvent: o objeto de evento;
+current: informação sobre a rota atual;
+previous: informações da rota anterior, ou undefined se for a primeira rota a ser invocada.
+
+*/
+application.run([
   "$location",
   "$rootScope",
-  
+
   function ($location, $rootScope) {
-    $rootScope.$on("$routeChangeSuccess", function(event, current, previous) {
+    $rootScope.$on("$routeChangeSuccess",function(event,current,previous){
       $rootScope.title = current.$$route.title;
-    });
+    })
   }
-]);
+])
