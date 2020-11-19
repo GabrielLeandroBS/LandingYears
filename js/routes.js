8719194effc5c86d83cd36d46d6e804ef34893b4
Gabriel Leandro <48261292+GabrielLeandroBS@users.noreply.github.com>
2020-11-19 20:05:50 -0300

    Update
    Atualização de todos os arquivos.

--- js/routes.js
+++ js/routes.js
@@ -5,8 +5,9 @@ application.config([
    function($routeProvider){
      $routeProvider.when("/",{
         title: "Template 2021 - 1",
-        templateUrl: "pages/main.htm",
-        controller: "newyear"
+        templateUrl: "pages/year.htm",
+        controller: "newyear",
+        css: "year.css"
      });
 
      $routeProvider.when("/year1",{
@@ -16,9 +17,11 @@ application.config([
        controller: "year1"
      })
 
-     $routeProvider.when("/contato",{
-       title: "Temaplate 2021 - 3",
-       templateUrl: "pages/contato.htm"
+     $routeProvider.when("/year2",{
+       title: "Template 2021 - 3",
+       templateUrl: "pages/year2.htm",
+       css: "year2.css",
+       controller: "year2"
      })
 
      .otherwise({ redirectTo: '/' });
@@ -26,7 +29,12 @@ application.config([
 ])
 
 application.controller("newyear",function ($scope) {
-  $scope.newyear = "HAPPY NEW YEAR"
+  $scope.newyear = "happy",
+  $scope.subtitle = "New Year",
+  $scope.conteudo = `Lorem ipsum dolor sit amet consectetur adipisicing e
+  lit. Adipiscinatus facilis at est quis nostrum! Tempora a dolores ea id quae
+  dignissimos doloremque praesentium ipsa, ex tempore iusto facilis
+  placeat.`
 })
 
 application.controller("year1",function($scope){
@@ -38,6 +46,14 @@ application.controller("year1",function($scope){
   placeat.`
 })
 
+application.controller("year2",function($scope){
+  $scope.newyear = "happy new year 2020",
+  $scope.conteudo = `Lorem ipsum dolor sit amet 
+  consectetur adipisicing elit. In quam
+   accusantium harum recusandae eligendi dicta 
+   nesciunt eos beatae voluptas id optio animi, dolore temporibus
+   asperiores. Beatae iste qui aut officiis?`
+})
 // No AngularJS o $rootScope é um objeto que permite que nós possamos armazenar todo o escopo de nossa aplicação, e em cada aplicação nós só podemos usar um e somente um $rootScope.
 // O serviço $ location analisa a URL na barra de endereço do navegador (com base em window.location ) e disponibiliza a URL para seu aplicativo
 /*
